bacec32f091bb28357aa9930a14683bb37c6d0d9
CWebler <acweblersantos@gmail.com>
2022-05-04 14:36:43 -0300

    adicionar PrimeiroPrograma.java

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -5,10 +5,47 @@ import br.com.dio.model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
-
         System.out.println(gato);
+
+        String nome;
+        Livro Livro = new Livro(nome:Object numPaginas;"O problema dos 3 corpos", numPaginas:300);
+
        /* int a = 5;
         int b = 3;
         System.out.println("Hello World!" + (a + b));*/
+    }
+}
+class Livro {
+
+    private String nome;
+    private Integer numPaginas;
+
+    Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
